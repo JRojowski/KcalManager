a93713e79b24e6fced7efb67d6002e0be62b6dcb
JRojowski <j.rojowski98@gmail.com>
2022-01-24 15:15:51 +0100

    Initial commit

---
+++ src/main/java/io/github/JRojowski/KcalManagerApplication.java
@@ -0,0 +1,13 @@
+package io.github.JRojowski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcalManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KcalManagerApplication.class, args);
+	}
+
+}
